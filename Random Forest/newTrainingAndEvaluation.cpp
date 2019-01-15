ac0d386701d4444bb287b2d58922e1cd6a81e7c2
Rahul Banerjee <38559682+thecomputationguy@users.noreply.github.com>
2019-01-15 13:06:38 +0100

    Updated the Training methods

--- Random Forest/newTrainingAndEvaluation.cpp
+++ Random Forest/newTrainingAndEvaluation.cpp
@@ -3,10 +3,15 @@
 //2. Evaluates a test set and returns the number of correctly classifies samples.
 
 #include <stdio.h>
-#include <opencv2/opencv.hpp>
+#include <tuple>
 #include <iostream>
+#include <random>
+#include <algorithm>
+#include <iterator>
+#include <opencv2/opencv.hpp>
 #include "opencv2/core/core.hpp"
 #include "opencv2/ml/ml.hpp"
+#include "newTrainingAndEvaluation.hpp"
 
 using namespace std;
 using namespace cv;
@@ -14,64 +19,76 @@ using namespace ml;
 
 float calculateAccuracy(Mat groundTruths, Mat predictions); //Just the implicit declaration
 
-class RForest
+tuple<Mat, Mat> subSampler(Mat features, Mat labels); //Just the implicit declaration
+
+
+// Creating multiple decision trees to generate a random forest
+
+void RForest::create(int n)
 {
-	public:
-            
-            vector<Ptr<DTrees>> model;
-            
-            // Creating multiple decision trees to generate a random forest
-            
-            void create(int n)
-            {
-                for int i = 0; i < n; i++
-                {
-                    Ptr<DTrees> model_instance = DTrees::create();
-                    model.push_back(model_instance);
-                }						
-            }
-            
-            // Training multiple DTrees and evaluating the best one based on accuracy
-            
-            Ptr<DTrees> train(Mat features, Mat labels)
-            {
-                for(int i = 0; i < n; i++)
-                {
-                    model[i]->setMaxDepth(200);
-                    model[i]->setCVFolds(0);
-                    model[i]->setMinSampleCount(15);
-                    model[i]->train(TrainData::create(features, ROW_SAMPLE, labels));
-                    cout<<endl<<"Tree Number: "<<n<<" trained!"<<endl;
-                }
-                
-                for(int i = 0; i < n; i++)
-                {
-                    int index;
-                    float accuracy = 0.0, best_accuracy = 0.0;
-                    
-                    Mat predicted_labels;
-                    
-                    test_features.convertTo(test_features, CV_32F);
-                    
-                    model[i]->predict(features, predicted_labels);
-                    
-                    accuracy = calculateAccuracy(labels, predicted_labels) * 100;
-                    
-                    if(accuracy > best_accuracy)
-                    {
-                        best_accuracy = accuracy;
-                        index = i;
-                    }
-                }
-                
-                cout<<"Best model is, dtree number: "<<index<<" and is saved."<<endl;
-                
-                model[index]->save("model.yml")
-                        
-                return model[index]
-            }       
-         
+    for (int i = 0; i < n; i++)
+    {
+        Ptr<DTrees> model_instance = DTrees::create();
+        model.push_back(model_instance);
+    }
 }
+            
+// Training multiple DTrees and evaluating the best one based on accuracy
+
+Ptr<DTrees> RForest::train(Mat features, Mat labels)
+{
+
+    Mat subsampledFeatures, subsampledLabels;
+
+    for (int i = 0; i < model.size(); i++)
+    {
+	tie(subsampledFeatures, subsampledLabels) = subSampler(features, labels); // Training on a random sub-sample
+        model[i]->setMaxDepth(200);
+        model[i]->setCVFolds(0);
+        model[i]->setMinSampleCount(15);
+        model[i]->train(TrainData::create(subsampledFeatures, ROW_SAMPLE, subsampledLabels));
+        cout<<endl<<"Tree Number: "<< (i + 1) <<" trained!"<<endl;
+    }
+    
+    int index = 0;
+
+    for (int i = 0; i < model.size(); i++)
+    {
+        cout<<"Evaluating Tree Number: "<<(i + 1)<<endl;
+        float accuracy = 0.0, best_accuracy = 0.0;
+        
+        Mat predicted_labels;
+        
+        features.convertTo(features, CV_32F);
+        
+        model[i]->predict(features, predicted_labels);
+        
+        accuracy = calculateAccuracy(labels, predicted_labels) * 100;
+
+	cout<<"Accuracy is: "<<accuracy<<"%"<<endl;
+        
+        if(accuracy > best_accuracy)
+        {
+            best_accuracy = accuracy;
+            index = i;
+        }
+    }
+    
+    cout<<"Best model is, DTree number: "<<index<<" and is saved."<<endl;
+    
+    model[index]->save("model.yml");
+    
+    return model[index];
+}
+
+Mat RForest::predict(Mat features) {
+    
+    //TODO:aggregate predictions from all the trees and vote for the best classification
+    //result as well as the confidence (percentage of votes for that winner class)
+    Mat result;
+    return result;
+}
+
                    
 
 // Calculates accuracy based on the ratio of correctly classified samples in the test set.
@@ -94,9 +111,78 @@ float calculateAccuracy(Mat groundTruths, Mat predictions)
 	}
 
 	cout<<"\n\nTest Data Evaluated!";
-	cout<<"\nTest Instances Correctly Classified: "<<counter<<" out of "<<total_labels;
+	cout<<"\nTest Instances Correctly Classified: "<<counter<<" out of "<<total_labels<<endl;
 
 	ratio = (counter/total_labels);
 
 	return ratio;
 }
+ // Function to select a random subset from the complete training data
+
+tuple<Mat, Mat> subSampler(Mat features, Mat labels)
+{
+	Mat sampledFeatures, sampledLabels, tmpFeature, tmpLabel;
+	vector<int> random_indices;
+	int index;
+	int num_original_samples = features.rows;
+	int num_reduced_samples = num_original_samples * 0.6; // Take 60% of the total training data
+
+	for(int i = 0; i < num_original_samples; i++)
+	{
+		random_indices.push_back(i);
+	}
+
+	// Shuffle the full taining data randomly and select the first 60% samples
+	random_shuffle(random_indices.begin(), random_indices.end());
+
+	for(int i = 0; i < num_reduced_samples; i++)
+	{
+		index = random_indices[i];
+		tmpFeature = (features.row(index) + 0);
+		tmpLabel = (labels.row(index) + 0);
+		sampledFeatures.push_back(tmpFeature);
+		sampledLabels.push_back(tmpLabel);
+	}
+
+	return make_tuple(sampledFeatures, sampledLabels);	
+	
+}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
